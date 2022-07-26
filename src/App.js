67fdbb46c99c44e5629a7d34b85fbd7ad7547382
JordBant <jaybant23@gmail.com>
2022-07-26 10:17:35 -0400

    Configuring styles and animations for gulp svg

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.scss'
 const App = () => {
   return (
     <div className="App">
-      
+      <TechSkill/>
     </div>
   );
 }
--- src/components/tech-skill/tech-skill-component.jsx
+++ src/components/tech-skill/tech-skill-component.jsx
@@ -1,12 +1,13 @@
 import './tech-skill.scss'
+import GulpIcon from '../skill-icons/gulpIcon/gulp-icon-component'
 
 const TechSkill = () => {
   return (
     <div className="skill-container">
+        <GulpIcon/>
+        {/* <div className="skills"></div>
         <div className="skills"></div>
-        <div className="skills"></div>
-        <div className="skills"></div>
-        <div className="skills"></div>
+        <div className="skills"></div> */}
     </div>
   )
 }
