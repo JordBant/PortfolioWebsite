9290965df60cb9cc9ff9779ef143c175e864c7ec
JordBant <jaybant23@gmail.com>
2022-07-25 23:31:59 -0400

    project setup

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+import TechSkill from "./components/tech-skill/tech-skill-component";
+import './App.scss'
+
 const App = () => {
   return (
     <div className="App">
---
+++ src/components/tech-skill/tech-skill-component.jsx
@@ -0,0 +1,14 @@
+import './tech-skill.scss'
+
+const TechSkill = () => {
+  return (
+    <div className="skill-container">
+        <div className="skills"></div>
+        <div className="skills"></div>
+        <div className="skills"></div>
+        <div className="skills"></div>
+    </div>
+  )
+}
+
+export default TechSkill
