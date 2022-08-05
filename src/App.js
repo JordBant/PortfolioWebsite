30def78432abf816278f11299a7a1846e820bd59
JordBant <jaybant23@gmail.com>
2022-08-05 18:37:29 -0400

    All skeleton pages added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import Credentials from "./components/routes/Credentials/credentials-page-component";
 import './App.scss'
 import About from "./components/routes/About/about-page-component";
+import NavBar from "./components/Nav/nav-bar/nav-bar-component";
+import Projects from "./components/routes/Projects/projects";
+import Contact from "./components/routes/Contact/contact";
 
 const App = () => {
   return (
     <div className="App">
+      <NavBar/>
       <About/>
       <Credentials/>
+      <Projects/>
+      <Contact/>
     </div>
   );
 }
