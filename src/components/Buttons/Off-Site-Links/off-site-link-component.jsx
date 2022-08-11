cc5f7f7b65989c84ffd355bb6921bff04d25156e
JordBant <jaybant23@gmail.com>
2022-08-11 13:39:04 -0400

    Link Icons

--- src/components/Buttons/Off-Site-Links/off-site-link-component.jsx
+++ src/components/Buttons/Off-Site-Links/off-site-link-component.jsx
@@ -1,8 +1,23 @@
 import React from 'react'
+import Github from '../../Icon-Component/github-component'
+import LinkedIn from '../../Icon-Component/linkdIn-component'
 
-const OffSiteLinkBtn = () => {
+const OffSiteLinkBtn = ({linkIcon}) => {
+  const switchIcon = (icon) => {
+    // eslint-disable-next-line default-case
+    switch (icon) {
+      case 'Github':
+        return <Github/>
+      case 'LinkedIn':
+        return <LinkedIn/>
+    }
+  }
   return (
-    <div>OffSiteLinkBtn</div>
+    <>
+      {
+        switchIcon(linkIcon)
+      }
+    </>
   )
 }
 export default OffSiteLinkBtn
