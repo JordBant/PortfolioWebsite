dc88495fbc5650c9fb73e666fbb6a850dbbd64f2
JordBant <jaybant23@gmail.com>
2022-08-15 12:43:39 -0400

    Button transition

--- src/components/Buttons/CTA-Button/cta-button-component.jsx
+++ src/components/Buttons/CTA-Button/cta-button-component.jsx
@@ -4,16 +4,14 @@ import './cta-button.scss'
 
 const CTAButton = ({variants, animate, initial}) => {
   return (
-    <div className='cta-button-wrapper' >
-      <motion.button 
-      className='cta-button'
-      variants={variants}
-      animate = {animate}
-      initial = {initial}
-      >  
-      <h1 className="inner-btn">Let's Connect</h1>
-      </motion.button>
-    </div>
+    <motion.button 
+    className='cta-button'
+    variants={variants}
+    animate = {animate}
+    initial = {initial}
+    >  
+      Let's Connect
+    </motion.button>
   )
 }
 
