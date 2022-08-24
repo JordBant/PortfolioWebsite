116dd71c51426f37828f447d0e136c98e6a2cd26
JordBant <jaybant23@gmail.com>
2022-08-24 10:14:17 -0400

    moving options lis

--- src/App.js
+++ src/App.js
@@ -28,7 +28,6 @@ const App = () => {
           ? <Loader/>
           :
           <>
-            <CustomCursor/>
             <NavBar/>
             <About backgroundActive= { backgroundActive }/>
             <Credentials/>
--- src/components/View-Credentials/view-credentials-component.jsx
+++ src/components/View-Credentials/view-credentials-component.jsx
@@ -4,11 +4,13 @@ import './view-credentials-component.scss'
 const ViewCredentials = () => {
   return (
     <div className="view-credential">
-        <p className="title">
-            {/* Download Resume PDF */}
-            View This Resume
-        </p>
-        <OptionsList/>
+      <div className="options-wrapper">
+          <OptionsList/>
+          <p className="title">
+              {/* Download Resume PDF */}
+              View This Resume
+          </p>
+      </div>
     </div>
   )
 }
