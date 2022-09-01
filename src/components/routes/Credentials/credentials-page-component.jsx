40eed14a28b2448d5e1f681445d89c8f2aa73970
JordBant <jaybant23@gmail.com>
2022-08-31 23:06:54 -0400

    Created new layout component

--- src/components/routes/Credentials/credentials-page-component.jsx
+++ src/components/routes/Credentials/credentials-page-component.jsx
@@ -1,5 +1,5 @@
+import SideTitleLayout from '../../Layout/Side-Titled-Container/side-titled-container-component'
 import PageTitle from '../../Page-Title/page-title-component'
-import SideTitle from '../../Side-Titles/side-title-component'
 import SkillCard from '../../Skill-Icons/skillCard-component'
 import './credentials-page.scss'
 
@@ -11,23 +11,25 @@ const Credentials = () => {
   ]
 
   return (
-    <section className='tech-skill-wrapper' >
-      <SideTitle iconNameArr={optionArr} title = 'View My Resume' />
-      <PageTitle pageTitleName = 'Skills' />
-      <div className="skill-icon-container">
-
-        <div className="skill-icon-list">
-          <SkillCard skillName = 'HTML' />
-          <SkillCard skillName = 'CSS' />
-          <SkillCard skillName = 'Javascript' />
-          <SkillCard skillName = 'React.js' />
-          <SkillCard skillName = 'SASS' />
-          <SkillCard skillName = 'Node.js' />
-          <SkillCard skillName = 'Gulp.js' />
-          <SkillCard skillName = 'Git' />
-          <SkillCard skillName = 'Express.js' />
-        </div>
-      </div>
+    <section className='credentials-main' >
+      <SideTitleLayout iconNameArr = {optionArr} title = 'View My Resume'>
+        <>
+          <PageTitle pageTitleName = 'Skills' />
+          <div className="skill-icon-container">
+            <div className="skill-icon-list">
+              <SkillCard skillName = 'HTML' />
+              <SkillCard skillName = 'CSS' />
+              <SkillCard skillName = 'Javascript' />
+              <SkillCard skillName = 'React.js' />
+              <SkillCard skillName = 'SASS' />
+              <SkillCard skillName = 'Node.js' />
+              <SkillCard skillName = 'Gulp.js' />
+              <SkillCard skillName = 'Git' />
+              <SkillCard skillName = 'Express.js' />
+            </div>
+          </div>
+        </>
+      </SideTitleLayout>
     </section>
   )
 }
