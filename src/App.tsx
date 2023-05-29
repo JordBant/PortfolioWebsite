4b63758389d992ca60ad5f4400e3aec3324ea746
JordBant <jaybant23@gmail.com>
2023-05-29 01:29:31 -0400

    Creating same page routing.

--- src/App.tsx
+++ src/App.tsx
@@ -6,23 +6,38 @@ import './styles/App.scss'
 function App() {
 
   return (
-      <div className="about-body-container">
-        <div className="about-body"
-        >
-            <h1 className="about-salutations"> I'm Jordon </h1>
-                <p className="about-body-text">
-                    I am a Frontend Web Developer with a Bachelors of Science in Computer Science. 
-                    I have a strong drive for creating robust, beautiful and 
-                    performant web applications and websites. 
-                </p>
-        </div>
-        {/* <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a> */}
-      </div>
+    <>
+    <header>
+      <nav>
+        <ul>
+          <li>About</li>
+          <li>Skills</li>
+          <li>Projects</li>
+        </ul>
+      </nav>
+    </header>
+      <div className='pages'>About</div>
+      <div className='pages'>Skills</div>
+      <div className='pages'>Projects</div>
+      <div className='pages'>Contact</div>
+    </>
+      // <div className="about-body-container">
+      //   <div className="about-body"
+      //   >
+      //       <h1 className="about-salutations"> I'm Jordon </h1>
+      //           <p className="about-body-text">
+      //               I am a Frontend Web Developer with a Bachelors of Science in Computer Science. 
+      //               I have a strong drive for creating robust, beautiful and 
+      //               performant web applications and websites. 
+      //           </p>
+      //   </div>
+      //   {/* <a href="https://vitejs.dev" target="_blank">
+      //     <img src={viteLogo} className="logo" alt="Vite logo" />
+      //   </a>
+      //   <a href="https://react.dev" target="_blank">
+      //     <img src={reactLogo} className="logo react" alt="React logo" />
+      //   </a> */}
+      // </div>
   )
 }
 
---
+++ src/pages/About/About.tsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+// type Props = {};
+
+export const About = (props: Props) => {
+  return (
+    <div className="about-body-container">
+        <div className="about-body"
+        >
+            <h1 className="about-salutations"> I'm Jordon </h1>
+                <p className="about-body-text">
+                    I am a Frontend Web Developer with a Bachelors of Science in Computer Science. 
+                    I have a strong drive for creating robust, beautiful and 
+                    performant web applications and websites. 
+                </p>
+        </div>
+        {/* <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a> */}
+    </div>
+  )
+}
