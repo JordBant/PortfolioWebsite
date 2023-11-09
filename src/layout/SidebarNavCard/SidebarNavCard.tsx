37f77038e21dcdfa3316b4e68ec1ea266a278c24
JordBant <jaybant23@gmail.com>
2023-11-08 20:56:01 -0500

    options list needs uni-directional animation on close

--- src/layout/SidebarNavCard/SidebarNavCard.tsx
+++ src/layout/SidebarNavCard/SidebarNavCard.tsx
@@ -2,21 +2,33 @@ import React, { FC, useCallback, useState } from 'react'
 import './SidebarNavCard.scss';
 import { Icon } from '../../components';
 import { SidebarNavCardProps } from './SidebarNavCard.types';
+import { SIDEBAR_ICONS } from '../../store/constants';
 
 export const SidebarNavCard: FC<SidebarNavCardProps> = ({projectName}) => {
-  const [isHovered, setIsHovered] = useState(false);
-  const isHoveredHandler = useCallback((toggleTo: boolean): undefined => {
-    setIsHovered(toggleTo)
+  const [isClicked, setisClicked] = useState(false);
+  const isClickedHandler = useCallback((toggleTo: boolean): undefined => {
+    setisClicked(toggleTo)
   }, []);
-  const activeMenuClassName: '' | 'sidebar-card__active-menu' = isHovered ? 'sidebar-card__active-menu' : ''
-  const activeOptionListClassName: '' | 'sidebar-card__active-options-list' = isHovered ? 'sidebar-card__active-options-list' : ''
+  const activeMenuClassName: '' | 'sidebar-card__active-menu' = isClicked ? 'sidebar-card__active-menu' : ''
+  const activeMask: '' | 'sidebar-card__active-mask' = isClicked ? 'sidebar-card__active-mask' : ''
+  const activeOptionListClassName: '' | 'sidebar-card__active-options-list' = isClicked ? 'sidebar-card__active-options-list' : ''
 
   return (
     <div className='sidebar-card'>
-      <div className='sidebar-card__sidebar'>
-        <Icon classNames='sidebar-card__options-btn' iconElement='KOBAB' onMouseLeave={() => isHoveredHandler(false)} onMouseEnter={() => isHoveredHandler(true)}/>
-        <div className={`sidebar-card__idk ${activeOptionListClassName}`}></div>
-        <div className={`sidebar-card__options-list ${activeMenuClassName}`}></div>
+      <div className={`sidebar-card__sidebar ${activeMenuClassName} ${activeMask}`}>
+        <Icon classNames={`sidebar-card__options-trigger ${activeMenuClassName}`} iconElement='KOBAB' onClick={() => isClickedHandler(!isClicked)} />
+        <h2 className="sidebar-card__project-name">
+          Project Name
+        </h2>
+      </div>
+      <div className={`sidebar-card__options-list ${activeOptionListClassName}`}>
+        <div className="sidebar-card__options-btn-wrapper">
+          {
+            SIDEBAR_ICONS.map((option) => (
+              <Icon iconElement={option} classNames='sidebar-card__options-btn'/>
+            ))
+          }
+        </div>
       </div>
     </div>
   )
