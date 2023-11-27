57e4cba88be70966864b769febee0c11526d5de6
JordBant <jaybant23@gmail.com>
2023-11-27 11:19:37 -0500

    Prettier stuff

--- src/layout/TextOverLay/TextOverLay.tsx
+++ src/layout/TextOverLay/TextOverLay.tsx
@@ -1,18 +1,17 @@
-import React, { FC } from 'react'
+import React, { FC } from 'react';
 import { Icon } from '../../components';
+
 import './TextOverlay.scss';
 
 type TextOverLayProps = {
-    title?: string;
-    children?: React.ReactElement
-    verticalLineColor?: string;
-}
+  title?: string;
+  children?: React.ReactElement;
+  verticalLineColor?: string;
+};
 
-export const TextOverLay: FC<TextOverLayProps> = ({children}) => {
-  return (
-    <div className='text-overlay-wrapper'>
-        <Icon iconElement={'VIDEO'}/>
-        {children}
-    </div>
-  )
-}
+export const TextOverLay: FC<TextOverLayProps> = ({ children }) => (
+  <div className="text-overlay-wrapper">
+    <Icon iconElement={'VIDEO'} />
+    {children}
+  </div>
+);
